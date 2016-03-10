b851d27b7b22a766adaefcd59f87c1ec6b38c1d8
Sujan <sujan@mckerchercorp.com>
2016-03-10 17:55:59 +0600

    added Instagram Access Token so that applicaiton works.

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.set('view engine', 'ejs');
 
 // configure instagram app with client-id
 ig.use({
+    // get access token here: http://instagram.pixelunion.net/
+    access_token: '2235677325.1677ed0.c010dfdd8f9542ac8a32779cd27a3e6a',
     client_id: 'e53c88191766433897d1e6e52d9f3749',
     client_secret: '00e5b333f5504651af822dcf4afc8558'
 });
