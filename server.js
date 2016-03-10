8ad3deca3c06d54c543effe48ba641a5bc591ad1
Sujan <sujan@mckerchercorp.com>
2016-03-10 15:53:49 +0600

    added express to the application and built an HTTP server.

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('Wow! A tiny Node app!! I am the happiest person in the world!!!');
+// grab express
+var express = require('express');
+
+// create an express app
+var app = express();
+
+// create an express route for the home page
+// http://localhost:8080/
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
+});
+
+//start the server on port 8080
+app.listen(8080);
+
+//send a message
+console.log('Server has started!');
